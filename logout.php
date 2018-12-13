80c52881a16c798ad7dc7dde37447bd1f6b85cd3
Sirawit Pongnakintr <plurm2545@hotmail.com>
2018-12-14 01:08:48 +0700

    Fix authentication

--- logout.php
+++ logout.php
@@ -2,7 +2,7 @@
     session_start();
     if(isset($_SESSION['user_id'])){
         unset($_SESSION['user_id']);
-        header("Location: index.php");
-        die();
     }
+    header("Location: index.php");
+    die();
 ?>
